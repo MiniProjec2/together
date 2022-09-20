145114d94a84140529d7bddbd78eefb19f8dc449
metroz1 <wlstjr132@gmail.com>
2022-09-20 15:51:46 +0900

    회원가입 시 아이디 중복 여부 체크 / 비밀번호 동일한지 체크 기능

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, jsonify, request
 app = Flask(__name__)
-
+import hashlib
 from pymongo import MongoClient
 client = MongoClient('mongodb+srv://text:sparta@cluster0.cvfqh2o.mongodb.net/cluster0?retryWrites=true&w=majority')
 db = client.dbsparta
@@ -13,6 +13,52 @@ def home():
 @app.route('/category/create')
 def postIndex():
     return render_template('postIndex.html')
+
+@app.route('/join')
+def join():
+    textInfo = [
+        {"text": "아이디 (영문과 숫자 및 일부 특수문자(._-), 2-10자 길이)",
+         "id": "user_id"
+         },
+        {"text": "비밀번호 (영문, 숫자는 필수이며 일부 특수 문자 사용 가능 8-20자 길이)",
+         "id": "user_pw"
+         },
+        {"text": "비밀번호 재입력",
+         "id": "user_pw_re"
+         },
+        {"text": "닉네임",
+         "id": "user_nickname"
+         },
+        {"text": "Github 주소 (선택)",
+         "id": "user_git"
+         }
+    ]
+    return render_template('join.html', textInfo=textInfo)
+
+@app.route('/join/id_check', methods=["POST"])
+def user_id_check():
+    idReceive = request.form['id_give']
+    exists = bool(db.users.find_one({"id": idReceive}))
+    return jsonify({'result:': 'success', 'exists': exists})
+
+@app.route('/join', methods=["POST"])
+def userRegister():
+    idReceive = request.form['id_give']
+    pwReceive = request.form['pw_give']
+    nicknameReceive = request.form['nickname_give']
+    gitReceive = request.form['git_give']
+
+    pwHash = hashlib.sha256(pwReceive.encode('utf-8')).hexdigest()
+
+    doc = {
+        'id': idReceive,
+        'pw': pwHash,
+        'nickname': nicknameReceive,
+        'git': gitReceive,
+    }
+    db.movies.insert_one(doc)
+
+    return jsonify({'msg': '저장 완료!'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
