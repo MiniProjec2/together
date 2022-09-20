4ecf21950ab5c0cf6d1297d8e4d285f54c573d94
ryucm <ckd12394@gmail.com>
2022-09-20 14:41:13 +0900

    first connect with postCreate

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ db = client.dbsparta
 ## HTML을 주는 부분
 @app.route('/')
 def home():
-   return render_template('index.html')
+    return render_template('index.html')
+
+@app.route('/category/create')
+def postIndex():
+    return render_template('postIndex.html')
 
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
